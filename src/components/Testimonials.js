be400315ab62e5f72622d9c069f41544a408a0fd
Daniel <dselasea@gmail.com>
2022-08-19 17:59:09 +0000

    Added testimonail component and testimonail styles

---
+++ src/components/Testimonials.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import testimonails from "./Testimonials.css"
+
+function Testimonials() {
+  return (
+    <div>Testimonials</div>
+  )
+}
+
+export default Testimonials
